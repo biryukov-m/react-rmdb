84c6beb9055022a9f5ea153b56efa7187243878e
Maxym <biryukov.mm@gmail.com>
2022-06-08 19:16:47 +0300

    Added global styles with styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+// Styles
+import { GlobalStyle } from './GlobalStyle';
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       Start here.
     </div>
   );
