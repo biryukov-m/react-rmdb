9de2ee8d85e8e27b3dd1e8147040e0bd62c9a4d3
Maxym <biryukov.mm@gmail.com>
2022-06-09 14:03:11 +0300

    Added HeroImage component. Added useHomeFetch custom hook and moved logic from Home.js.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
-
+import HeroImage from './HeroImage';
 // Hook
-
+import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    const { state, loading, error } = useHomeFetch();
 
-    return <div>Homepage</div>
+    console.log(state);
+    return (
+        <>
+            {state.results[0] ?
+                <HeroImage
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                />
+                : null
+            }
+        </>
+    )
 };
 
 export default Home;
