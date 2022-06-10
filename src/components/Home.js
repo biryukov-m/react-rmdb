3e2ade102d11d138bb3d5a679c0b2ac1c03ac831
Maxym <biryukov.mm@gmail.com>
2022-06-10 22:25:13 +0300

    Added logic for component Button, now it loads more movies, when clicked. 3:19:32

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,9 +14,18 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
+    const {
+        state,
+        loading,
+        error,
+        searchTerm,
+        setSearchTerm,
+        setIsLoadingMore
+    } = useHomeFetch();
 
     console.log(state);
+
+    if (error) return <div>Something went wrong =(</div>;
 
     return (
         <>
@@ -46,7 +55,7 @@ const Home = () => {
             </Grid>
             {loading && <Spinner />}
             {state.page < state.total_pages && !loading && (
-                <Button text='Load more' />
+                <Button text='Load more' callback={() => setIsLoadingMore(true)} />
             )}
         </>
     )
