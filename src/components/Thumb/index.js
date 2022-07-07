74c831081c8c1994df8305c80674c402fc93a51e
Maxym <biryukov.mm@gmail.com>
2022-07-07 13:41:39 +0300

    Added Link components into Header and Thumb components

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Image } from './Thumb.styles';
+import { Link } from 'react-router-dom';
 
 
 const Thumb = ({ image, movieId, clickable }) => (
     <div>
-        <Image src={image} alt="movie-thumb" />
+        {clickable ? (
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt="movie-thumb" />
+            </Link>
+        ) : (
+            <Image src={image} alt="movie-thumb" />
+        )}
     </div>
 );
 
