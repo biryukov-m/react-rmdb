957c1890d06ab4e4aac4e85d1e053a07b07c14d6
Maxym <biryukov.mm@gmail.com>
2022-07-07 16:47:17 +0300

    Added custom useMovieFetch custom hook, that takes data about Movie from id given in url (router);

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+// Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+// Hook
+import { useMovieFetch } from "../hooks/useMovieFetch";
+// Image
+import NoImage from '../images/no_image.jpg';
 
-const Movie = () => <div>Movie</div>;
+const Movie = () => {
+    const { movieId } = useParams();
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+    console.log(movie);
+    return (
+        <>
+            <div>{movieId}</div>
+        </>
+    );
+};
+
 
 export default Movie;
