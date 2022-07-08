47268293888808ac5390924b64d16c7cb3cacf98
Maxym <biryukov.mm@gmail.com>
2022-07-08 11:22:18 +0300

    Added BreadCrumb component with styles which is used in Movie component.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
+import BreadCrumb from "./BreadCrumb";
 import Grid from './Grid';
 import Spinner from './Spinner';
 // Hook
@@ -14,8 +15,11 @@ const Movie = () => {
     const { movieId } = useParams();
     const { state: movie, loading, error } = useMovieFetch(movieId);
     console.log(movie);
+    if (loading) return <Spinner />;
+    if (error) return <div>Something went wrong.</div>;
     return (
         <>
+            <BreadCrumb movieTitle={movie.original_title} />
             <div>{movieId}</div>
         </>
     );
